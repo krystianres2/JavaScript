11bf09b2d286b94781327ac2ee504050b92f4758
krystianres2 <91950213+krystianres2@users.noreply.github.com>
2023-05-09 18:33:29 +0200

    Add files via upload

--- Nowy folder/script.js
+++ Nowy folder/script.js
@@ -1,152 +1,137 @@
-let list = document.getElementById("list");
-let radio1 = document.getElementById("radio1");
-let radio2 = document.getElementById("radio2");
-let AIcb = document.getElementById("cb1");
-let Systemycb = document.getElementById("cb2");
-let Językicb = document.getElementById("cb3");
-let Androidcb = document.getElementById("cb4");
-let Egzamincb = document.getElementById("cb5");
-let Zerówkacb = document.getElementById("cb6");
-let Kolokwiumcb = document.getElementById("cb7");
-let Projektcb = document.getElementById("cb8");
-
-let button = document.getElementById("button");
-
-
-
-class Row {
-    constructor(date, name, type) {
-        this.date = date;
-        this.name = name;
-        this.type = type;
-    }
-    display() {
-        let string = "Data: " + this.date.toLocaleDateString() + " Nazwa: " + this.name + " Rodzaj zaliczenia: " + this.type;
-        return string;
+//TODO: Tworzymy klasę Coin zawierającą wszystkie niezbędne pola (wyświetlane później w tabeli) 
+//class Coin
+//TODO: Tworzymy tablicę listOfCoins, do której przypiszemy wszystkie pobrane z API coiny. 
+//const listOfCoins;
+let input=document.getElementById("input")
+let input2=document.getElementById("input2")
+let button=document.getElementById("button")
+button.addEventListener("click", sprawdz);
+function sprawdz(){
+    let tem1=input.value;
+    let tem2=input2.value;
+    let tem3 = tem1+"%2B"+tem2;
+    
+    fetchData(tem3)
+}
+class Coin{
+    constructor(name, country){
+        
+        this.name=name;
+        this.country=country
     }
 }
+async function fetchData(xd){
+    const url ='https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions?location=%2B'+xd+'&radius=100';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd2f437d3ffmsh2d6fc063ae7a7fbp1b1a83jsn708210271d98',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
 
-const obj1 = new Row(new Date(2023, 04, 22), "Sztuczna inteligencja", "Zerówka");
-const obj2 = new Row(new Date(2023, 04, 26), "Systemy operacyjne", "Egzamin");
-const obj3 = new Row(new Date(2023, 04, 30), "Systemy operacyjne", "Kolokwium");
-const obj4 = new Row(new Date(2023, 05, 05), "Sztuczna inteligencja", "Egzamin");
-const obj5 = new Row(new Date(2023, 05, 20), "Sztuczna inteligencja", "Kolokwium");
-const obj6 = new Row(new Date(2023, 05, 25), "Systemy operacyjne", "Projekt");
-const obj7 = new Row(new Date(2023, 05, 26), "Języki internetowe", "Projekt");
-const obj8 = new Row(new Date(2023, 05, 27), "Języki internetowe", "Kolokwium");
-const obj9 = new Row(new Date(2023, 06, 3), "Programowanie urządzeń mobilnych", "Projekt");
-const obj10 = new Row(new Date(2023, 06, 3), "Programowanie urządzeń mobilnych", "Kolokwium");
-
-let defaultLIst = []
-defaultLIst.push(obj1)
-defaultLIst.push(obj2)
-defaultLIst.push(obj3)
-defaultLIst.push(obj4)
-defaultLIst.push(obj5)
-defaultLIst.push(obj6)
-defaultLIst.push(obj7)
-defaultLIst.push(obj8)
-defaultLIst.push(obj9)
-defaultLIst.push(obj10)
-
-
-for (let i = 0; i < defaultLIst.length; i++) {
-    let li = document.createElement("li");
-    list.appendChild(li);
-    list.lastChild.innerHTML = defaultLIst[i].display();
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+    displayData(JSON.parse(result))
+} catch (error) {
+	console.error(error);
+}
+    
 }
 
-button.addEventListener("click", implement);
+async function fetchCoinDetails(coinId){
+    console.log(coinId);
+    const url = `https://coinranking1.p.rapidapi.com/coin/${coinId}?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '92cafd2032msh75252a91a66893ap1128bdjsn75fcdc8c70ba',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+    };
 
-function implement() {
-    const ul = document.querySelector('ul');
-    ul.innerHTML = '';
-    // var tbody = document.querySelector("#myTable tbody");
-    // let tbody = document.querySelector("#myTableBody");
-    // tbody.innerHTML = "";
-    let temp = defaultLIst.slice();
-
-    if (!AIcb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].name == "Sztuczna inteligencja") {
-                temp.splice(i, 1);
-            }
-        }
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
     }
-    if (!Systemycb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].name == "Systemy operacyjne") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Językicb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].name == "Języki internetowe") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Androidcb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].name == "Programowanie urządzeń mobilnych") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Egzamincb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].type == "Egzamin") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Zerówkacb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].type == "Zerówka") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Kolokwiumcb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].type == "Kolokwium") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-    if (!Projektcb.checked) {
-        for (let i = temp.length - 1; i >= 0; i--) {
-            if (temp[i].type == "Projekt") {
-                temp.splice(i, 1);
-            }
-        }
-    }
-
-    if (radio1.checked) {
-        temp.sort((a, b) => a.date - b.date);
-    }
-    if (radio2.checked) {
-        temp.sort((a, b) => b.date - a.date);
-
-    }
-    for (let i = 0; i < temp.length; i++) {
-        let li = document.createElement("li");
-        list.appendChild(li);
-        list.lastChild.innerHTML = temp[i].display();
-    }
-
-    // for (var i = 0; i < temp.length; i++) {
-    //     var row = tbody.insertRow();
-    
-    //     var dateCell = row.insertCell();
-    //     nameCell.textContent = temp[i].date.toLocaleDateString();
-    
-    //     var nameCell = row.insertCell();
-    //     dateCell.textContent = temp[i].name;
-    
-    //     var typeCell = row.insertCell();
-    //     typeCell.textContent = temp[i].type;
-    // }
 }
+//fetchCoinDetails("Qwsogvtv82FCd");
+// fetchData();
+
+const mainDivElement = document.getElementById("manDiv");
+const coinsTableElement = document.getElementById("coinsData");
+
+function displayData(result){
+    //TODO:Pobieramy liste/tablice obiektow typu Coin. I dodajemy wszystkie do listOfCoins.
+    //Następnie przesyłamy tablicę listOfCoins do metody createTable.
+    console.log(result)
+
+    let listOfCoins=[];
+    result.data.forEach(coin =>{
+        const temp=new Coin(coin.name, coin.country);
+        listOfCoins.push(temp);
+    } )
+
+    // createInfoBar(result.data.stats);
+    createTable(listOfCoins);
+}
+
+function createInfoBar(stats){
+    const infoBarElement = document.getElementById("infoBar");
+    infoBarElement.innerHTML = `
+        <span>Cryptos <a href="#">${stats.totalCoins}</a></span>
+        <span>Exchanges <a href="#">${stats.totalExchanges}</a></span>
+        <span>24h Volume <a href="#">${stats.total24hVolume}</a></span>    
+        <span>Market Cap <a href="#">${stats.totalMarketCap}</a></span>    
+    `;
+}
+
+function createTable(coinsList){
+    coinsList.forEach(coin => {
+        coinsTableElement.innerHTML += 
+        `
+        <tr class="coinRow">
+                    <td>${coin.name}</td>
+                    <td>${coin.country}</td>
+                </tr>
+        `
+        // const formattedPrice = new Intl.NumberFormat('en-US',
+        //     { style: 'currency', currency: 'USD' }
+        //     ).format(coin.price);
+        // const formattedMarketCap = new Intl.NumberFormat('en-US',
+        //     { style: 'currency', currency: 'USD' }
+        //     ).format(coin.marketCap);
+        // const formattedChange = parseFloat(coin.change);
+        // let additionalClass;
+
+        // if(formattedChange > 0){
+        //     additionalClass = "greenText";
+        // }
+        // else if (formattedChange == 0){
+
+        //     additionalClass = "";
+        // }else{
+        //     additionalClass = "redText";
+        // }
+
+        // coinsTableElement.innerHTML += 
+        // `
+        // <tr id="${coin.uuid}" class="coinRow">
+        //             <td><i class="fa-regular fa-star"></i></td>
+        //             <td>${coin.rank}</td>
+        //             <td><img src="${coin.iconUrl}"> ${coin.name}</td>
+        //             <td class="${additionalClass}">${formattedPrice}</td>
+        //             <td class="${additionalClass}">${coin.change}</td>
+        //             <td>${formattedMarketCap}</td>
+        //         </tr>
+        // `
+    })
+}
+
+
 
